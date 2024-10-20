17c53f4426ac89cbae8961b5f1b47eade61f8b00
Ankit Sinha <ankitsinha.vkit@gmail.com>
2024-10-21 00:34:22 +0530

    initial commit

---
+++ src/main/java/com/kinky_story_generation/KinkyStoryGeneration/KinkyStoryGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.kinky_story_generation.KinkyStoryGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinkyStoryGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinkyStoryGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinky_story_generation/KinkyStoryGeneration/KinkyStoryGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinky_story_generation.KinkyStoryGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinkyStoryGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
